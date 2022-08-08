15e6be5ff076229f70dacfa9541f78524c39c4ba
NetUnit <andriyproniyk@gmail.com>
2022-08-08 21:10:01 +0300

    Lesson 7: Redefine Post Model + Update Form

--- SpringBootcamp/src/main/java/com/example/demo/controllers/BlogController.java
+++ SpringBootcamp/src/main/java/com/example/demo/controllers/BlogController.java
@@ -6,10 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Returns the array with all blog posts for our db
@@ -39,12 +50,12 @@ public class BlogController {
 
          for (Field field : attributes) {
              // Dynamically read Attribute Name
-             System.out.println("ATTRIBUTE NAME: " + field.getName());
+             // System.out.println("ATTRIBUTE NAME: " + field.getName());
 
              // gets object field-value
              field.setAccessible(true);
              Object objectValue = field.get(object);
-             System.out.println("ATTRIBUTE Value: " + objectValue);
+             // System.out.println("ATTRIBUTE Value: " + objectValue);
          }
 
           // single object
@@ -61,21 +72,145 @@ public class BlogController {
     public String blogAdd(Model model) {
         return "blog-add";
     }
-
     @PostMapping("/blog/add")
     public String blogAddForm(
             @RequestParam String title,
             @RequestParam String anons,
             @RequestParam String full_text,
-            Model model)
-    {
-        System.out.println(String.format(
-                "These are form @params: title %s | anons %s | full_text %s", title, anons, full_text)
-        );
+            HttpServletRequest request,
+            Model model) throws IOException {
+        // System.out.println(String.format(
+        //        "These are form @params: title %s | anons %s | full_text %s", title, anons, full_text)
+        // );
+        // form is an object outlined by our Model
+        // that is being saved into the db
         Post form = new Post(title, anons, full_text);
-
-        System.out.println(form);
         postRepository.save(form);
+
+        // Check POST results of Create blog object
+        String method = String.format("request method: %s", request.getMethod());
+        String url = String.format("%s", request.getRequestURL());
+        int status = getHttpRequestStatus(url);
+        // basic POST result output
+        System.out.println(String.format("%s | request url: %s | request status: %d", method, url, status));
+
+        // Check full POST request result
+        String request_data = httpServletRequestToString(request);
+        System.out.println(request_data);
+
         return "redirect:/blog";
+    }
+
+    // in order to make this url dynamic with dynamic url_pattern
+    // @PathVariable decorator is needed which takes dynamic id parameter
+    @GetMapping("/blog/{id}")
+    public String blogDetailed(
+            Model model,
+            @PathVariable(value = "id") Integer id,
+            HttpServletRequest request,
+            HttpServletResponse response) throws IOException {
+        // existsById - searches if such id presented in the db
+        if(!postRepository.existsById(id)) {
+            // this will raise 404 status
+            raise404(request, response);
+            //return "redirect:/blog";
+        }
+
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> post_attrs = new ArrayList<>();
+        post.ifPresent(post_attrs::add);
+        System.out.println(post_attrs);
+        model.addAttribute("post_attrs", post_attrs);
+        return "blog-detail";
+    }
+
+    // update functionality
+    // @GetMapping decorator aims to use POST/GET methods along with each other
+    @GetMapping("/blog/{id}/delete")
+    public String blogDelete(Model model) {
+        return "blog-update";
+    }
+    @GetMapping("/blog/{id}/update")
+    public String blogUpdateForm(
+            Model model,
+            @PathVariable(value = "id") Integer id,
+            HttpServletRequest request,
+            HttpServletResponse response) throws IOException {
+
+        if(!postRepository.existsById(id)) {
+            // this will raise 404 status
+            raise404(request, response);
+        }
+
+        // get post obj from db
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> post_attrs = new ArrayList<>();
+        post.ifPresent(post_attrs::add);
+        model.addAttribute("post_attrs", post_attrs);
+        return "blog-update";
+    }
+    @PostMapping("/blog/{id}/update")
+    public String blogUpdateForm(
+            Model model,
+            @RequestParam String title,
+            @RequestParam String anons,
+            @RequestParam String full_text,
+            @PathVariable(value = "id") Integer id,
+            HttpServletRequest request,
+            HttpServletResponse response) throws IOException {
+
+        Post upd_post = postRepository.findById(id).orElseThrow();
+        // through setter methods we assign new object attributes
+        upd_post.setTitle(title);
+        upd_post.setAnons(anons);
+        upd_post.setFull_text(full_text);
+        // check obtained result of posted data
+         System.out.println(String.format(
+                "This is posted data: post_obj - %s | title: %s | anons: %s | full_text: %s ",
+                upd_post, title, anons, full_text)
+         );
+        postRepository.save(upd_post);
+        return "redirect:/blog";
+    }
+    private String httpServletRequestToString(HttpServletRequest request) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Request Method = [" + request.getMethod() + "], ");
+        sb.append("Request URL Path = [" + request.getRequestURL() + "], ");
+
+        String headers =
+                Collections.list(request.getHeaderNames()).stream()
+                        .map(headerName -> headerName + " : " + Collections.list(request.getHeaders(headerName)) )
+                        .collect(Collectors.joining(", "));
+
+        if (headers.isEmpty()) {
+            sb.append("Request headers: NONE,");
+        } else {
+            sb.append("Request headers: ["+headers+"],");
+        }
+
+        String parameters =
+                Collections.list(request.getParameterNames()).stream()
+                        .map(p -> p + " : " + Arrays.asList( request.getParameterValues(p)) )
+                        .collect(Collectors.joining(", "));
+
+        if (parameters.isEmpty()) {
+            sb.append("Request parameters: NONE.");
+        } else {
+            sb.append("Request parameters: [" + parameters + "].");
+        }
+        return sb.toString();
+    }
+    private int getHttpRequestStatus(String request_url) throws IOException {
+        URL url = new URL(request_url);
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.connect();
+
+        int code = connection.getResponseCode();
+        return code;
+    }
+    public void raise404(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.sendError(HttpServletResponse.SC_NOT_FOUND);
     }
 }
--- SpringBootcamp/src/main/java/com/example/demo/models/Post.java
+++ SpringBootcamp/src/main/java/com/example/demo/models/Post.java
@@ -17,9 +17,26 @@ public class Post {
         return id;
     }
 
-    public void setId(Integer Id) {
-        this.id = id;
+    public String getTitle() {
+        return title;
     }
+    public String getAnons() {
+        return anons;
+    }
+    public String getFull_text() {
+        return full_text;
+    }
+    public int getViews() {
+        return views;
+    }
+
+
+    public void setId(Integer Id) {this.id = id;}
+    public void setTitle(String title) {this.title = title;}
+    public void setAnons(String anons) {this.anons = anons;}
+    public void setFull_text(String full_text) {this.full_text = full_text;}
+    public void setViews(int views) {this.views = views;}
+
 
     // after we've assigned paramaters in Controller(Blog)
     // init is obligatory
